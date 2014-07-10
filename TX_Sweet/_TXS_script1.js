8b5bee2dae7407a70645b527d3d22ee85623a653
SiemperCode <amhippely@gmail.com>
2014-07-10 13:56:26 -0500

    TXSweet 1.1

---
+++ TX_Sweet/_TXS_script1.js
@@ -0,0 +1,8 @@
+﻿var closeUp = function (picId) {
+    var pic = document.getElementById(picId).getAttribute('src');
+    document.getElementById('modalPic').setAttribute('src', pic);
+
+    $('#myModal').modal();
+  
+}
+$('#myModal').modal('hide');
